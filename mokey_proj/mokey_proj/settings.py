039f3df9eb3c350937be8b19a69116458d139957
ys <ys.heo.korea@gmail.com>
2022-01-20 10:19:37 +0900

    local working

--- mokey_proj/mokey_proj/settings.py
+++ mokey_proj/mokey_proj/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rv@abhk9)4aq!y_h7gfw24hi5l#&#lhecdg!mv*^sn@gk^$r81'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
