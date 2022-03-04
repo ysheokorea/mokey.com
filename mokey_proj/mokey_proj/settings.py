09191ccacfaeb713ac19b7d2cce9ea8df126d851
ys <ys.heo.korea@gmail.com>
2022-03-04 20:44:58 +0900

    password issue closed

--- mokey_proj/mokey_proj/settings.py
+++ mokey_proj/mokey_proj/settings.py
@@ -145,6 +145,6 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'ystestservice@gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_PASSWORD ='rnrmfxptmxm11!'
+EMAIL_HOST_PASSWORD ='Rnrmfxptmxm11!'
 
 
